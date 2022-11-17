8aa4d5a37ffe93df0005481bb55b87a3c2e0861d
OlhaDorofiychuk <olha.d@yahoo.com>
2022-11-17 16:35:02 +0000

    start

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import { useState } from "react"
-import "./styles.css"
+import { useState } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import "./styles.css";
+import Dashboard from "./pages/Dashboard";
+import PersonProfile from "./pages/PersonProfile";
 
 export default function App() {
-  const [hiredPeople, setHiredPeople] = useState([])
+  const [hiredPeople, setHiredPeople] = useState([]);
+
+  function hirePerson(person) {
+    setHiredPeople([...hiredPeople, person]);
+  }
 
   return (
     <>
@@ -14,6 +21,13 @@ export default function App() {
           </ul>
         </nav>
       </header>
+      <Routes>
+        <Route path="/" element={<Dashboard hiredPeople={hiredPeople} />} />
+        <Route
+          path="/view/:id"
+          element={<PersonProfile hirePerson={hirePerson} />}
+        />
+      </Routes>
     </>
-  )
+  );
 }
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,10 +1,20 @@
-import { useState } from "react"
-import PeopleList from "./components/PeopleList"
+import { useState } from "react";
+import PeopleList from "./components/PeopleList";
+import { useEffect } from "react";
 
 function Dashboard(props) {
-  const { hiredPeople } = props
+  const { hiredPeople } = props;
 
-  const [people, setPeople] = useState([])
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api?results=50")
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data.results);
+        setPeople(data.results);
+      });
+  }, []);
 
   return (
     <main className="dashboard-layout">
@@ -17,7 +27,7 @@ function Dashboard(props) {
         <PeopleList people={hiredPeople} />
       </section>
     </main>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
